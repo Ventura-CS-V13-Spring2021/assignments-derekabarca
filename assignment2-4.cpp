821d16bec51ab82d289cbce3d864b2b6849ccc39
derekabarca <54419486+derekabarca@users.noreply.github.com>
2021-03-02 11:05:10 -0800

    Create assignment2-4.cpp

---
+++ assignment2-4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    char c;
+    int i = 65;
+
+    while (i <= 90){
+
+        c = i;
+        if (c % 5 == 4)
+        {
+            cout << c << " " << endl;
+        }
+
+        else
+        {
+            cout << c << " ";
+        }
+
+        i++;
+    }
+
+
+}

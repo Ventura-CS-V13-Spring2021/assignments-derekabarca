e324789aa39b1520ebfcb315ae62de7ecbb93a64
derekabarca <54419486+derekabarca@users.noreply.github.com>
2021-03-22 21:45:59 -0700

    Create Quiz2-2.cpp
    Question 3

---
+++ Quiz2-2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+     int number1, number2, number3;
+     cout << "Enter three integer values : " ;
+     cin >> number1 >> number2 >> number3 ;
+     cout << number1 << ", " << number2 << ", " << number3 << ". " << endl;
+
+
+     if (number1 != number2 && number1 != number3 && number2 != number3)
+     cout << " There is no duplicated number. ";
+
+     if (number1 == number2 && number1 == number3)
+     cout << " All numbers are the same. ";
+
+     if (number1 == number2)
+     cout << "First two values are duplicates. ";
+
+     if (number1 == number3)
+     cout << " First and third values are duplicates. ";
+
+     if (number2 == number3)
+     cout << " Second and third values are duplicates. ";
+
+
+
+     return 0;
+}

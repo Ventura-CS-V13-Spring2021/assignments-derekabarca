03e5c484144bd8a7381c5479a9aac35424db60c7
derekabarca <54419486+derekabarca@users.noreply.github.com>
2021-03-22 21:54:50 -0700

    Question 5

---
+++ quiz2-2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int num, i, range1, range2;
+
+    cout << "Enter your range from 1-100:\n ";
+    cin >> range1 >> range2;
+    cout << "Range: " << "[" << range1 << " , " << range2 << "]" << endl;
+
+    for (num = range1; num <= range2; num++){
+        for (i = 2; i<num; i++){
+            if (num % 100 == 0)
+                break;
+        }
+        if (i == num)
+            cout << num << " is a prime number. \n";
+        else
+            cout << num << " is not a prime number. \n";
+    }
+    
+    return 0;
+}

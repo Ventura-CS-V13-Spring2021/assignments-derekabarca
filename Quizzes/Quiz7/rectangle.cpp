06aea5cdbb62293ce9a6b591ea5e53bae8413e42
derekabarca <>
2021-05-11 03:22:22 +0000

    3rd

--- Quizzes/Quiz7/rectangle.cpp
+++ Quizzes/Quiz7/rectangle.cpp
@@ -13,11 +13,33 @@ Rectangle::Rectangle()
 Rectangle::Rectangle(Coordinate lbval, Coordinate rtval): lb(lbval), rt(rtval)
 {
 }
-Rectangle::Coordinate getLB() const
+Coordinate Rectangle::getLB() const
 {
   return lb;
 }
-Rectangle::Coordinate getRT() const
+Coordinate Rectangle::getRT() const
 {
   return rt;
+}
+double Rectangle::getArea()
+{
+  return area;
+}
+Coordinate Rectangle::getCenter()
+{
+  return center;
+}
+void Rectangle::setLBRT(Coordinate lbval, Coordinate rtval)
+{
+  lbval = lb;
+  rtval = rt;
+}
+void Rectangle::printRectangle() const
+{
+  cout << "Bottom left point: "
+  lb.printXY();
+  cout << endl;
+  cout << "Top right point: "
+  rt.printXY();
+  cout << endl;
 }

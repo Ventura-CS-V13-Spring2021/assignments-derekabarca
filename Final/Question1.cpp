0ceeeb243da831f1cbbda9d29c33db16fedfff0c
derekabarca <>
2021-05-18 02:33:57 +0000

    10th, Final Q1

--- Final/Question1.cpp
+++ Final/Question1.cpp
@@ -11,11 +11,32 @@ int max = 0;
 for(int i=0; i<6; i++)
 {
   int current = getNumDiv(arr, arr[i], 6);
-  if(current > max
+  if(current > max)
   {
     max = current;
   }
 }
+cout << endl << "Element 6 has the greatest number of factors, which is " << max << endl;
 return 0;
+}
+int getNumDiv(int arr[], int i, int length)
+{
+  int count = 0;
+cout << endl << "# of factors for " << i << ": ";
+for(int d = 1; d < i; d++)
+{
+    if(i % d == 0)
+{
+for(int c = 0; c < length; c++)
+{
+    if(d == arr[c])
+{
+count++;
+cout << arr[c] << ", ";
+}
+}
+}
+}
+return count;
 }
 

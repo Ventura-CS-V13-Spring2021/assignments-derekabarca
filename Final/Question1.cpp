734343166090bcfa08d07f47e35ad77539f972a0
derekabarca <>
2021-05-18 00:50:50 +0000

    3rd

--- Final/Question1.cpp
+++ Final/Question1.cpp
@@ -19,5 +19,19 @@ int main(){
 
 int getNumDiv(int x[], int elms)
 {
-  int 
+  int mostFactors;
+  int highestNum;
+  int factors = 0;
+
+  for(int i = 0; i<elms, i++)
+  {
+    factors = 0;
+    for (int j = 1; j<x[i]; j++)
+    {
+      if (x[i]%j == 0)
+      {
+        
+      }
+    }
+  }
 }

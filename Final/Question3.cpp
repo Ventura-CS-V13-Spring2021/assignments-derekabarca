9854aea2a7d368bd3cea38dd89c350f0450040af
derekabarca <>
2021-05-18 04:18:04 +0000

    New 1st

--- Final/Question3.cpp
+++ Final/Question3.cpp
@@ -1,45 +1,24 @@
 #include <iostream> 
 #include <string>
+#include <cstdlib>
+#include <iomanip>
 using namespace std;
 
-class Student 
+class Student
 {
-  public:
-  Student();
-  Student(int num, string name);
-  Student(Student &rhs);
-
-
-  private:
-  string name; 
-  int num_classes; 
-  string* class_list;  
-
+public: Student();
+        ~Student();
+        void InputData();
+        void OutputData();
+        void Reset();
+        Student& operator =(const Student& rightSide);
+  
+private:
+    string name;
+    int num_classes;
+    string *class_list;
 };
 
-Student::Student() : name(), num_classes(0)
-{
-  classList = new string[num_classes]; 
-}
-
-Student::Student(string n, int num) : name(n), num_classes(num)
-{
-  classList = new string[num_classes];
-   for (int i = 0; i < num_classes; i++)
-   {
-       cout << "Enter class name : ";
-       cin >> classList[i];
-   }
-}
-
-Student::Student(const Student& rhs)
-{
-  name = rhs.name; 
-  num_classes = rhs.num_classes; 
-  classList = new string[num_classes]; 
-  for (int i=0; i<num_classes; i++)
-    classList[i] = rhs.classList[i]; 
-} 
 
 
 int main(){

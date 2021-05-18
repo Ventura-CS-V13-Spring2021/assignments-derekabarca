5ddb480258a520501cd19259851706af06ae5e1f
derekabarca <>
2021-05-18 02:20:13 +0000

    9th

--- Final/Question1.cpp
+++ Final/Question1.cpp
@@ -3,3 +3,19 @@
 
 using namespace std;
 
+int getNumDiv(int arr[], int i, int len);
+int main(){
+
+int arr[] = {1, 2, 4, 6, 10, 24};
+int max = 0;
+for(int i=0; i<6; i++)
+{
+  int current = getNumDiv(arr, arr[i], 6);
+  if(current > max
+  {
+    max = current;
+  }
+}
+return 0;
+}
+

bc51ee6f472a4c50520fe392ccfc042628655615
derekabarca <>
2021-05-18 01:10:58 +0000

    5th

---
+++ Final/Question2.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+using namespace std; 
+
+void sort(int arr[], int length); 
+
+int main(){
+
+  int arr[] = {-10, 10, 1, 2, 5, -13, 12, 15, -6, 10, 2, 5};
+  int count = 1;
+
+  cout << "N " << " Count" << endl;
+  for(int i=0; i<sort())
+
+
+}
+

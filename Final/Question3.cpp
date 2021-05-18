f31344490f5ed2767176569ab64c57b1afb6b352
derekabarca <>
2021-05-18 02:53:35 +0000

    11th

--- Final/Question3.cpp
+++ Final/Question3.cpp
@@ -1,3 +1,23 @@
 #include <iostream> 
 #include <string>
 using namespace std;
+
+class Student 
+{
+  public:
+
+
+
+  private:
+  string name; 
+  int num_classes; 
+  string* class_list;  
+
+};
+
+
+int main(){
+
+
+
+}
